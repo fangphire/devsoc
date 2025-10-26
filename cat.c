1fd370fbc60913bb592a0b12bc33299f9258ffef
Fang <f20251024@goa.bits-pilani.ac.in>
2025-10-26 09:53:42 +0000

    Finished cat.c

--- cat.c
+++ cat.c
@@ -25,32 +25,51 @@ int main(int argc, char *argv[]) {
     // TODO: Step 1 - Check for the correct number of command-line arguments.
     // If argc is not equal to 2, print a usage message to stderr
     // (e.g., "Usage: %s <filename>\n") and exit with a status of 1.
- 
+     if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
  
     // TODO: Step 2 - Open the file.
     // The filename is in argv[1].
+
+    char *filepath = argv[1];
+    int fd;
     // Use the open() system call to open it in read-only mode (O_RDONLY).
     // Store the returned file descriptor in an integer variable.
     // If open() returns -1, an error occurred. Use perror("Error opening file")
     // to print the error and exit with a status of 1.
- 
+    fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1;
+    }
  
     // TODO: Step 3 & 4 - Read from the file and write to standard output.
     // Create a buffer (an array of chars) of size BUFFER_SIZE.
+    char arr[BUFFER_SIZE];
     // Use a loop with the read() system call to read from the file into the buffer.
+    int BytesRead;
     // The read() call returns the number of bytes read.
     // The loop should continue as long as read() returns a value greater than 0.
+    while ((BytesRead = read(fd, arr, BUFFER_SIZE)) > 0){
     // Inside the loop, use the write() system call to write the buffer's contents
     // to standard output (STDOUT_FILENO). The number of bytes to write is the
     // value returned by read().
+        int bytesWritten = write(STDOUT_FILENO, arr, BytesRead);
     // Be sure to handle potential errors from both read() and write().
- 
- 
+        if (bytesWritten == -1) {
+        perror("Error writing to stdout");
+        close(fd);
+        return 1;
+    }
     // TODO: Step 5 - Close the file.
     // Use the close() system call to close the file descriptor.
     // Handle any potential errors from close().
- 
- 
+        if (close(fd) == -1) {
+        perror("Error closing file");
+        return 1;
+    }
     return 0; // Return 0 for success.
 }
  
